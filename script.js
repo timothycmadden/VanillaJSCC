e4897741e2c6b7853bac74a59804825936c4ee6b
Timothy Madden <timcmadden@gmail.com>
2025-04-25 14:04:53 -0400

    linear and radial gradient

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ canvas.width = 700;
 canvas.height = 900;
 
 // global canvas settings
-ctx.strokeStyle = 'magenta';
+const gradient1 = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient1.addColorStop('0.2', 'pink');
+gradient1.addColorStop('0.3', 'red');
+gradient1.addColorStop('0.4', 'orange');
+gradient1.addColorStop('0.5', 'yellow');
+gradient1.addColorStop('0.6', 'green');
+gradient1.addColorStop('0.7', 'turquoise');
+gradient1.addColorStop('0.8', 'violet');
+const gradient2 = ctx.createRadialGradient(canvas.width * 0.5, canvas.height  * 0.5, 30, canvas.width * 0.5, canvas.height * 0.5, 400);
+gradient2.addColorStop('0.2', 'turquoise');
+gradient2.addColorStop('0.4', 'violet');
+gradient2.addColorStop('0.6', 'blue');
+ctx.strokeStyle = gradient2;
 
 class Line {
   constructor(canvas) {
@@ -18,9 +30,14 @@ class Line {
 
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.history = { x: this.x, y: this.y };
+    this.history = [{ x: this.x, y: this.y }];
     this.lineWidth = Math.floor(Math.random() * 15 + 1);
-    this.hue = Math.floor(Math.random() * 360);
+    // this.hue = Math.floor(Math.random() * 360);
+    this.maxLength = Math.floor(Math.random() * 150 + 10);
+    this.speedX = Math.random() * 5 - 0.5;
+    this.speedY = 7;
+    this.lifeSpan = this.maxLength * 3;
+    this.timer = 0;
   }
 
   draw(context) {
@@ -37,11 +54,46 @@ class Line {
     }
     context.stroke();
   }
+
+  update() {
+    this.timer++;
+    if (this.timer < this.lifeSpan) {
+      this.x += this.speedX + Math.random() * 20 - 10;
+      this.y += this.speedY + Math.random() * 20 - 10;
+      this.history.push({ x: this.x, y: this.y });
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length <= 1) {
+      this.reset();
+    } else {
+      this.history.shift();
+    }
+  }
+
+  reset() {
+    this.x = Math.random() * this.canvas.width;
+    this.y = Math.random() * this.canvas.height;
+    this.history = [{ x: this.x, y: this.y }];
+    this.timer = 0;
+  }
 }
 
 const linesArray = [];
-const numberOfLines = 1;
+const numberOfLines = 200;
 for (let i = 0; i < numberOfLines; i++) {
   linesArray.push(new Line(canvas));
 }
 linesArray.forEach((line) => line.draw(ctx));
+
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  linesArray.forEach((line) => {
+    line.draw(ctx);
+    line.update();
+  });
+
+  requestAnimationFrame(animate);
+};
+
+animate();
